51807b1cb8386263d453e89662b10a22a25d8d5b
ruzar7789-cpu <ruzar7789@gmail.com>
2026-07-14 18:27:32 +0200

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Aegis Security Auditor v2.0',
+  description: 'Profesionální modul pro pasivní recon a bezpečnostní audity',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="cs">
+      <body className="antialiased">{children}</body>
+    </html>
+  )
+}
